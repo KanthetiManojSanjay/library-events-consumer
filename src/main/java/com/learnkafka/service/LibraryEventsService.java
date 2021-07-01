d2f5ff7828f08dcb4db317192f462cd431343441
kansanja <sanjay.kantheti@e.lloydsbanking.com>
2021-07-01 13:37:34 +0530

    Initial commit

---
+++ src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -0,0 +1,104 @@
+package com.learnkafka.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.jpa.LibraryEventsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.RecoverableDataAccessException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class LibraryEventsService {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    private LibraryEventsRepository libraryEventsRepository;
+
+    public void process(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("libraryEvent: {}", libraryEvent);
+
+        if (libraryEvent.getLibraryEventId() != null && libraryEvent.getLibraryEventId() == 000) {
+            throw new RecoverableDataAccessException("Temporary Network Issue");
+        }
+
+        switch (libraryEvent.getLibraryEventType()) {
+            case NEW:
+                //save operation
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                //update operation
+                validate(libraryEvent);
+                save(libraryEvent);
+                break;
+            default:
+                log.info("Invalid Library Event Type");
+        }
+    }
+
+    private void validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.getLibraryEventId() == null) {
+            throw new IllegalArgumentException("Library EventId is missing");
+        }
+        Optional<LibraryEvent> libraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+        if (!libraryEventOptional.isPresent()) {
+            throw new IllegalArgumentException("Not a valid library Event");
+        }
+        log.info("Validation is successful for the library Event : {}", libraryEventOptional.get());
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully persisted library Event {}", libraryEvent);
+    }
+
+    public void handleRecovery(ConsumerRecord<Integer, String> consumerRecord) {
+
+        Integer key = consumerRecord.key();
+        String message = consumerRecord.value();
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, message);
+        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, message, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, message, result);
+            }
+        });
+
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending the message and the exception is {}", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error is onFailure: {}", throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message sent Successfully for the key : {} and the value is {}, partition is {}", key, value, result.getRecordMetadata().partition());
+    }
+
+}
